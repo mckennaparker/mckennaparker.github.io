6861cadb07cfdc94f1cb7e2726bc67e6cc56caa3
McKenna Parker <mckennarparker@gmail.com>
2026-06-09 18:40:20 -0400

    Update routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Nav from './components/Nav.tsx'
 import App from './App.tsx'
 import './App.css'
@@ -11,17 +11,21 @@ import HoudiniLearningLogFoundationsVFX from './pages/blogs/HoudiniLearningLogFo
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Nav />}>
           <Route index element={<App />} />
           <Route path="projects" element={<Portfolio />} />
-          <Route path="blog" element={<Blog />} />
+
+          <Route path="blog">
+            <Route index element={<Blog />} />
+            <Route path="houdini-learning-log-foundations-welcome" element={<HoudiniLearningLogFoundationsWelcome />} />
+            <Route path="houdini-learning-log-foundations-vfx" element={<HoudiniLearningLogFoundationsVFX />} />
+          </Route>
+
           <Route path="resume" element={<div><h1>Resume</h1></div>} />
-          <Route path="/blog/houdini-learning-log-foundations-welcome" element={<HoudiniLearningLogFoundationsWelcome />} />
-          <Route path="/blog/houdini-learning-log-foundations-vfx" element={<HoudiniLearningLogFoundationsVFX />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
